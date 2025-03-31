71d7aa4d560f17e16ad8627230c88a93f488ccfa
ABHINEET <abhineet1805@gmail.com>
2025-03-31 22:55:29 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
-    'import.meta.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL),
+  server: {
+    port: 3000,
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  }
 })
